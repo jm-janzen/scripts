e4551cea7702aa8f6d896bfb9cc73a2299288e5b
jm-janzen <jmjanzen@gmail.com>
2016-03-13 23:45:15 +0000

    Beautify output of diffs

--- difffiles.sh
+++ difffiles.sh
@@ -1,11 +1,17 @@
-#!/bin/sh -e
+#!/bin/bash
 # highlight differences between common files in two given directories
 
 if [ $1 ] && [ $2 ]; then
-    diff --suppress-blank-empty \
-      --suppress-common-lines   \
-      --ignore-all-space        \
-      $1 $2 | grep -v Only | grep -E '^[diff].+|$' --color
+
+    cmd="diff --suppress-blank-empty --suppress-common-lines --ignore-all-space"
+
+    if [[ $3 == *"-u"* ]]; then
+        cme=" --unified=0"
+    fi
+
+      cmf=" $1 $2 | grep -vE '^Only|^---|^\+\+\+|@@\s' |  sed 's/^diff.* /\nFILE: /g'"
+      echo Issuing Command: \`$cmd $cme $cmf\'
+      eval $cmd $cme $cmf
 else
     echo 'Usage: ./this <dir1> <dir2>'
 fi
