3aa0f3d58b7b6874f59bb86a44ea53624a62d05a
jm-janzen <jmjanzen@gmail.com>
2016-03-13 16:45:28 +0000

    Add parameter checking and Usage help on incorrect usage
    Usage: ./this <dir1> <dir2>

--- difffiles.sh
+++ difffiles.sh
@@ -1,7 +1,12 @@
 #!/bin/sh -e
 # highlight differences between common files in two given directories
 
-diff --suppress-blank-empty \
-    --suppress-common-lines \
-    --ignore-all-space      \
-    $1 $2 | grep -v Only | grep -E '^[diff].+|$' --color
+if [ $1 ] && [ $2 ]; then
+    diff --suppress-blank-empty \
+      --suppress-common-lines   \
+      --ignore-all-space        \
+      $1 $2 | grep -v Only | grep -E '^[diff].+|$' --color
+else
+    echo 'Usage: ./this <dir1> <dir2>'
+fi
+
