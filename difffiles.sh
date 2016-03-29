795f9ebf2e3ac06d5a22c2cfb1cdc1df2c0dcc15
jm-janzen <jmjanzen@gmail.com>
2016-03-29 15:34:57 -0500

    Update directory diff script to set defaults
    While here, add help/usage text parameter

--- difffiles.sh
+++ difffiles.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 # highlight differences between common files in two given directories
 
-if [ $1 ] && [ $2 ]; then
-
-    cmd="diff --suppress-blank-empty --suppress-common-lines --ignore-all-space"
-
-    if [[ $3 == *"-u"* ]]; then
-        cme=" --unified=0"
-    fi
-
-      cmf=" $1 $2 | grep -vE '^Only|^---|^\+\+\+|@@\s' |  sed 's/^diff.* /\nFILE: /g'"
-      echo Issuing Command: \`$cmd $cme $cmf\'
-      eval $cmd $cme $cmf | GREP_COLOR="0;32" egrep --color=always '^<.*|^\+.*|$' \
-        | GREP_COLOR="0;31" egrep --color=always '^>.*|^\-.*|$'
-else
+if [[ $1 == *"-h"* ]]; then
     echo 'Usage: ./this <dir1> <dir2> [opts]'
     echo ' -u'
     echo '  unified diff, rather than split into columns'
+    exit 0
+elif [ -z $1 ] || [ -z $2 ]; then
+    here=${HOME}
+    there=${HOME}/env
+else
+    here=$1
+    there=$2
 fi
 
+cmd="diff --suppress-blank-empty --suppress-common-lines --ignore-all-space"
+
+if [[ $3 == *"-u"* ]]; then
+    cme=" --unified=0"
+fi
+
+# finally:
+
+cmf=" $here $there | grep -vE '^Only|^---|^\+\+\+|@@\s' |  sed 's/^diff.* /\nFILE: /g'"
+echo Issuing Command: \`$cmd $cme $cmf\'
+eval $cmd $cme $cmf | GREP_COLOR="0;32" egrep --color=always '^<.*|^\+.*|$' \
+  | GREP_COLOR="0;31" egrep --color=always '^>.*|^\-.*|$'
