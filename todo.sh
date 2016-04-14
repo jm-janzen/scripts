5410f310a586ab82bbc8206f56888a30b5c9b8c0
jm-janzen <jmjanzen@gmail.com>
2016-04-13 22:25:23 -0500

    Summarize tally of found items at the end

--- todo.sh
+++ todo.sh
@@ -2,10 +2,25 @@
 
 # scan files for patterns "XXX, TODO, WIP"
 
-egrep -R --binary-file=without-match \
+egrep -Rn --binary-file=without-match \
     --exclude-dir=.git $1 \
     --exclude-dir=.npm \
     --exclude-dir=node_modules \
-    | awk '/TODO|XXX|WIP/{
+    | awk '
+    BEGIN {
+        todo = 0;
+        xxx  = 0;
+        wip  = 0;
+    }/TODO/{
+        todo++
         print $0
-    }'
+    }/XXX/{
+        xxx++
+        print $0
+    }/WIP/{
+        wip++
+        print $0
+    } END {
+        printf("TODO:%d, XXX:%d, WIP:%d\n", todo, xxx, wip);
+    }' \
+    | less -R
