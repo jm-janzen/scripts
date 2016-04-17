9cf6e068b6a3aed82a7922f97847a30bd07ba095
jm-janzen <jmjanzen@gmail.com>
2016-04-16 22:49:51 -0500

    Lengthen separators and remove debug print

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh -e
 # Format output of alias print dump into more readable format
 
 alias | awk -F'=' 'BEGIN {
         print "ALIAS                         | COMMAND";
-        print "------------------------------------";
+        print "---------------------------------------";
     }
     {
-        print "moo";
         baselen  = 10;
         fieldlen = length($1);
         tablen   = baselen - fieldlen;
@@ -19,5 +18,5 @@ alias | awk -F'=' 'BEGIN {
         printf "| " $s "\n"
     }
     END {
-        print "------------------------------------";
+        print "---------------------------------------";
     }'
