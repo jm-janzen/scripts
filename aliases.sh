86e4b46d3a4a91450ca1757837c8e00b0c71765b
jm-janzen <jmjanzen@gmail.com>
2016-04-16 22:33:35 -0500

    Whitespace police action
    git diff -b relatively clean

--- aliases.sh
+++ aliases.sh
@@ -2,22 +2,22 @@
 # Format output of alias print dump into more readable format
 
 alias | awk -F'=' 'BEGIN {
-    print "ALIAS                         | COMMAND";
-    print "------------------------------------";
-}
-{
-    print "moo";
-    baselen  = 10;
-    fieldlen = length($1);
-    tablen   = baselen - fieldlen;
+        print "ALIAS                         | COMMAND";
+        print "------------------------------------";
+    }
+    {
+        print "moo";
+        baselen  = 10;
+        fieldlen = length($1);
+        tablen   = baselen - fieldlen;
 
-    printf " " $1
+        printf " " $1
 
-    for (i = 0; i < tablen; i++)
-        printf " "
+        for (i = 0; i < tablen; i++)
+            printf " "
 
-    printf "| " $s "\n"
-}
-END {
-    print "------------------------------------";
-}'
+        printf "| " $s "\n"
+    }
+    END {
+        print "------------------------------------";
+    }'
