c1467d21d232954c2abce35990f754060d08b493
jm-janzen <jmjanzen@gmail.com>
2016-04-30 10:53:10 -0500

    Bash script to follow, filter, single-line ip connection logs
    Summary
      crawl.sh - log file follower, filterer.
    
    Usage
      ./crawl.sh [file]
    
    Description
      Follow updates on a connection-type log file,
      ignoring connections from the same IP address
      that started this script.
    
    Bash was necessary for -f (check if file) assessment

---
+++ crawl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+#
+# Summary
+#   crawl.sh - log file follower, filterer.
+#
+# Usage
+#   ./crawl.sh [file]
+#
+# Description
+#   Follow updates on a connection-type log file,
+#   ignoring connections from the same IP address
+#   that started this script.
+#
+
+if [[ -f $1 ]]; then
+    log_file=$1
+else
+    # If multiple log files, just use first found
+    log_file=`find . -iname '*.log' -print -quit`
+fi
+
+conn_ip=$(echo $SSH_CLIENT | awk '{ print $1 }')
+
+printf "Following log file %s, ignoring %s\n" $log_file $conn_ip
+
+tail -f ${log_file} | grep -v ${conn_ip}
