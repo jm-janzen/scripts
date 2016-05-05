0b896694f38d69db8219149d4f0000edf9ef354b
jm-janzen <jmjanzen@gmail.com>
2016-05-05 18:03:36 -0500

    Simple nodejs script to query public events
    Just a fun little exercise

---
+++ gevents.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var request = require('request');
+
+var opts = {
+    uri: 'https://api.github.com' + '/users/jm-janzen/events',
+    json: true,
+    method: 'GET',
+    headers: {
+        'User-Agent': 'nodejs script'
+    }
+}
+
+request(opts, function (err, res, body) {
+    if (err) throw new Error(err);
+
+    var format = '[%s]: %s %s %s %s';
+
+    for (var i = 0; i < body.length; i++) {
+        console.log(format
+            , body[i].created_at
+            , body[i].type
+            , body[i].actor.login
+            , body[i].repo.name);
+    }
+});
