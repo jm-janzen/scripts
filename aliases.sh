ed4d195f6f61d1a2e046157056626dbe6d78121d
jm-janzen <jmjanzen@gmail.com>
2016-05-07 13:38:55 -0500

    Add check for line continuation char before printing alias command
    This block still requires some TLC - doesn't pair well with GNU
    screen sessions.

--- aliases.sh
+++ aliases.sh
@@ -4,6 +4,7 @@
 awk -F= 'BEGIN {
         TABLE_FMT = "%-21s | %-59s";
         row = sprintf(TABLE_FMT, "ALIAS", "COMMAND");
+        holdover;
         print row;
         gsub(".", "-", row);
         print row;
@@ -13,7 +14,15 @@ awk -F= 'BEGIN {
         # replace all multi-spaces with a single space
         gsub(/\s+/, " ", $0);
 
-        printf(TABLE_FMT "\n", $1, $2);
+        # check for continuation char
+        if ($2 ~ /\\$/) {
+            holdover = $1 $2;
+        } else if (holdover) {
+            printf(TABLE_FMT holdover, "\n", $1, $2, "\n");
+            holdover = 0;
+        } else {
+            printf(TABLE_FMT "\n", $1, $2);
+        }
 
     } END {
         print row;
