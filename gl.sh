7b2858e2af9e9fd131898655068c5868c6bdf543
jm-janzen <jmjanzen@gmail.com>
2016-05-19 18:34:56 -0500

    Exit on any kind of error

--- gl.sh
+++ gl.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 ### summarize a particular user's contributions to a repository.
 if [ $1 ]; then
     git log --shortstat --author "$1" | grep "files\? changed" | awk '{f+=$1; i+=$4; d+=$6} END {print "files:", f, "green:", i, "red", d}'
