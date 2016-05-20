e99feb6d00fe1e62e09760b669836917479dd78d
jm-janzen <jmjanzen@gmail.com>
2016-05-19 20:48:38 -0500

    Add full SUD comment block
    As per main format.

--- ipinfo.sh
+++ ipinfo.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
+#
+# Summary
+#   ipinfo.sh - simple query to API for info on an IP address.
+#
+# Usage
+#   ./ipinfo.sh [address]
+#
+# Description
+#   Returns info on hostname, city, region, country, lat-long coordinates,
+#   organization, and mail code, all in JSON format.  If an IP address is
+#   not specified, the API returns information on the address making the
+#   request (and this script prints it dutifully).
+#
+
 eval curl ipinfo.io/"${1}"/json
 
 echo # push \n
