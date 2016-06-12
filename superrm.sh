970b98e27cf31038f0a31580d30ca0d3df31abc7
jm-janzen <jmjanzen@gmail.com>
2016-06-11 22:22:09 -0500

    Add SUD comment block to superrm.sh convenience helper script

--- superrm.sh
+++ superrm.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -e
-# convenience script to show summary of a directory before prompting to delete it
+
+#
+# Summary
+#   superrm.sh - Show summary of a directory before prompting to delete it.
+#
+# Usage
+#   ./superrm.sh <dir>
+#
+# Description
+#   Show a GNU tree summary of a directory and its contents before prompting
+#   invoking user to to recursively delete its contents.
+#
 
 if [ $1 ]; then
     tree $1 -aC | less --RAW-CONTROL-CHARS --quit-if-one-screen --squeeze-blank-lines --chop-long-lines
