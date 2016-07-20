bbb6eb69d845cf8c5f8181661f36dd5a721f329c
JM <jmjanzen@gmail.com>
2016-07-20 13:06:21 -0400

    Notify must be run as root, and allow variable proc names
    Default is 'gulp'.
    Switch from dash -> bash.

--- gulp-tracer.sh
+++ gulp-tracer.sh
@@ -1,4 +1,14 @@
-#!/bin/sh -e
-# Usage: `sudo sh gulp-tracer.sh'
+#!/usr/bin/env bash
 
-strace -tp $(pgrep -l gulp | awk '{ print $1}') -etrace=stat -esignal=none
+# Usage: ./gulp-tracer.sh [name]
+
+proc='gulp'
+
+if [[ $UID != 0 ]]; then
+    echo 'Script must be run as root (sudo)'
+    exit 1
+elif [[ $1 ]]; then
+    proc=$1
+else
+    strace -tp "$(pgrep -l gulp | awk '{ print $1}')" -etrace=stat -esignal=none
+fi
