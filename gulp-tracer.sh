f6c7f92bf428ac86127f4b7a8d35684eb80d5c40
JM <jmjanzen@gmail.com>
2016-07-20 13:18:11 -0400

    Fix bug where passing parm would not run strace

--- gulp-tracer.sh
+++ gulp-tracer.sh
@@ -2,13 +2,16 @@
 
 # Usage: ./gulp-tracer.sh [name]
 
-proc='gulp'
+function traceProc {
+    echo "Running strace for $1"
+    strace -tp "$(pgrep -l $(echo "$1") | awk '{ print $1}')" -etrace=stat -esignal=none
+}
 
 if [[ $UID != 0 ]]; then
     echo 'Script must be run as root (sudo)'
     exit 1
 elif [[ $1 ]]; then
-    proc=$1
+    traceProc $1
 else
-    strace -tp "$(pgrep -l gulp | awk '{ print $1}')" -etrace=stat -esignal=none
+    traceProc 'gulp'
 fi
