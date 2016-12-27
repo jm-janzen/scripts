8cdba7b21c92ed0d179ee049aa52d6b6ae0c8ef2
JM <jmjanzen@gmail.com>
2016-12-26 21:50:59 -0500

    Add SUD comment block to gclone-ssh.sh convenience script

--- gclone-ssh.sh
+++ gclone-ssh.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+#
+# Summary
+#   gclone-ssh.sh - Convenience script to quickly clone my own repositories
+#
+# Usage
+#   ./gclone-ssh.sh
+#
+# Description
+#   Using the magic String of `owner,' clone the given repository by name,
+#   from github.com.  Relies on git being properly configured for this owner.
+#
+
+
 owner="jm-janzen"
 
 if [ -z $1 ]; then
