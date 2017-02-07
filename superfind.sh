a84f81c2837a4812408650b51e91d8765736261e
JM <jmjanzen@gmail.com>
2017-02-07 09:37:36 -0500

    Make dir parm optional - PWD as default

--- superfind.sh
+++ superfind.sh
@@ -4,12 +4,14 @@
 #   superfind.sh - convenience script for GNU find package
 #
 # Usage
-#   ./superfind.sh <dir> <filename>
+#   ./superfind.sh [dir] <filename>
 #
 
-if [ $1 ]; then
-  find $1 -iname $2 -printf '%p\n'
+if [ $# -gt 1 ]; then
+  find $1 -iname \*$2\* -printf '%p\n'
+elif [[ $# -gt 0 ]]; then
+  find . -iname \*$1\* -printf '%p\n'
 else
-  echo 'Usage: ./this <dir> <filename>'
+  echo 'Usage: ./superfind.sh [dir] <filename>'
 fi
 
